a4ca0d9628ef62092b2c68d1bbef6f13b446beb8
Vinicius Kirst <vinicius.kirst@gmail.com>
2022-07-03 13:54:00 -0300

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# nomad_cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: nomad_cookbook
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
