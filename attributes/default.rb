73fd6d51f08e5db4056d5734010459dc0413d172
Vinicius Kirst <vinicius.kirst@gmail.com>
2022-07-03 20:02:25 -0300

    initial script

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+
+default['nomad']['version'] = '1.3.1'
+default['nomad']['architecture'] = 'arm64'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,67 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+
+user 'nomad' do
+
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/nomad.zip" do
+    source "https://releases.hashicorp.com/nomad/#{node['nomad']['version']}/nomad_#{node['nomad']['version']}_linux_#{node['nomad']['architecture']}.zip"
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+    not_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/nomad.zip") }
+end
+
+archive_file 'nomad' do
+    path "#{Chef::Config[:file_cache_path]}/nomad.zip"
+    destination '/tmp/'
+    action :extract
+    not_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/nomad") }
+end
+
+bash 'move_nomad' do
+    cwd '/usr/local/bin'
+    code <<-EOH
+        cp /tmp/nomad /usr/local/bin/nomad
+    EOH
+    not_if { ::File.exist?('/usr/local/bin/nomad') }
+end
+
+template '/etc/systemd/system/nomad.service' do
+    source 'nomad.service.erb'
+    owner 'root'
+    group 'root'
+    mode '0755'
+end
+
+directory '/etc/nomad.d' do
+    owner 'root'
+    group 'root'
+    mode '0700'
+end
+
+template '/etc/nomad.d/nomad.hcl' do
+    owner 'root'
+    group 'root'
+end
+
+template '/etc/nomad.d/server.hcl' do
+    owner 'root'
+    group 'root'
+end
+
+template '/etc/nomad.d/client.hcl' do
+    owner 'root'
+    group 'root'
+end
+
+service 'nomad.service' do
+    supports status: true, restart: true, reload: true
+    action [:enable, :start]
+end
+
+
+
