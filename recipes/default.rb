a2b6ded6594b18f047b94e0b2a9121db16a00098
ckrst <58567534+ckrst@users.noreply.github.com>
2024-02-05 02:53:29 -0300

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,7 +18,7 @@ end
 
 archive_file 'nomad' do
     path "#{Chef::Config[:file_cache_path]}/nomad.zip"
-    destination '/tmp/nomad'
+    destination '/tmp/'
     action :extract
     not_if { ::File.exist?("/tmp/nomad") }
 end
